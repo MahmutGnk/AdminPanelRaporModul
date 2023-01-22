41ed75913ba06373081cd23074fdde0de1a56ca8
MahmutGnk <119791322+MahmutGnk@users.noreply.github.com>
2023-01-22 19:11:59 +0300

    AdminPanelRaporModul

---
+++ AdminPanelRaporModul/AdminPanelRaporModul/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using AdminPanelRaporModul.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Microsoft.Data.SqlClient;
+
+namespace AdminPanelRaporModul.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+        SqlConnection con = new SqlConnection();
+        List<Site> sites = new List<Site>();
+
+        private readonly ILogger<HomeController> _logger;
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        public HomeController(ILogger<HomeController> logger)
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        {
+            _logger = logger;
+            con.ConnectionString = AdminPanelRaporModul.Properties.Resources.ConnectionString;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        private void Fetchdata()
+        {
+            try
+            {
+                con.Open();
+                com.Connection = con;
+                com.CommandText = "SELECT TOP (1000) [product_id],[quantity],[price],[total],[tax],[currencyType],[product_supplier],[date],[musteri],[musteri_id] FROM [AdminPanelRaporModul].[dbo].[order_product_list]";
+                dr=com.ExecuteReader();
+                
+                con.Close();
+            }
+            catch (Exception)
+            {
+                
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AdminPanelRaporModul/AdminPanelRaporModul/Models/Site.cs
@@ -0,0 +1,18 @@
+﻿namespace AdminPanelRaporModul.Models
+{
+    public class Site
+    {
+        public int product_id { get; set; }
+        public int quantity { get; set; }
+        public float price { get; set; }
+        public float total { get; set; }
+        public float tax { get; set; }
+        public string? CurrencyType { get; set; }
+
+        public string? product_supplier { get; set; }
+        public DateOnly date { get; set; }
+        public string? musteri { get; set; }
+        public int musteri_id { get; set; }
+
+    }
+}
